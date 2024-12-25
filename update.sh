0dfb002b5ed40c31cbbc82f6029503c642362917
Zoey <zoey@z0ey.de>
2024-12-25 20:13:37 +0100

    Update update.sh
    Signed-off-by: Zoey <zoey@z0ey.de>

--- update.sh
+++ update.sh
@@ -5,63 +5,53 @@ if [ -z "$TZ" ] || ! echo "$TZ" | grep -q "^[A-Za-z/]\+$"; then
     sleep inf
 fi
 
-if [ -z "$DUK" ] || ! echo "$DUK" | grep -q "^[A-Za-z0-9]\+$"; then
-    echo "DUK is unset or invalid, it can consist of upper letters A-Z, lower letters a-z and numbers 0-9."
-    sleep inf
-fi
-
-
-if [ "$IPv4" = "false" ] || [ "$(curl -s4 ipv64.net; echo $?)" != "7" ]; then
-    if ! echo "$IPv4" | grep -q "^true$\|^false$"; then
-        echo "IPv4 needs to be true or false."
-        sleep inf
-    fi
-else
-  if [ "$IPv4" != "false" ]; then
-    if ! echo "$IPv4" | grep -q "^true$\|^false$"; then
-      echo "IPv4 needs to be true or false."
-      sleep inf
-    fi
-    echo "IPv4 does not work, disabling it."
-    export IPv4="false"
-  fi
-fi
-
-
-if [ "$IPv6" = "false" ] || [ "$(curl -s6 ipv64.net; echo $?)" != "7" ]; then
-    if ! echo "$IPv6" | grep -q "^true$\|^false$"; then
-        echo "IPv6 needs to be true or false."
-        sleep inf
-    fi
-else
-  if [ "$IPv6" != "false" ]; then
-    if ! echo "$IPv6" | grep -q "^true$\|^false$"; then
-      echo "IPv6 needs to be true or false."
-      sleep inf
-    fi
-    echo "IPv6 does not work, disabling it."
-    export IPv6="false"
-  fi
-fi
-
-
 if ! echo "$UI" | grep -q "^[0-9]\+[smhd]\?$"; then
     echo "UI needs to be a number which can be followed by one of the chars s, m, h or d."
     sleep inf
 fi
+
+if [ -z "$DUK" ] || ! echo "$DUK" | grep -q "^[A-Za-z0-9]{32\}$"; then
+    echo "DUK is unset or invalid, it can consist of 32 of the follwoing characters, upper letters A-Z, lower letters a-z and numbers 0-9."
+    sleep inf
+fi
+
+if [ -z "$DUD" ] || ! echo "$DUD" | grep -q "\."; then
+    echo "DUD is unset or invalid, it needs to contain at least one dot."
+    sleep inf
+fi
+
+if ! echo "$IPv4" | grep -q "^true$\|^false$"; then
+    echo "IPv4 needs to be true or false."
+    sleep inf
+fi
+
+if ! echo "$IPv6" | grep -q "^true$\|^false$"; then
+    echo "IPv6 needs to be true or false."
+    sleep inf
+fi
+
+if ! curl -sS4 ipv4.ipv64.net -o /dev/null; then
+    echo "IPv4 does not work, disabling it."
+    export IPv4="false"
+fi
+
+if ! curl -sS6 ipv6.ipv64.net -o /dev/null; then
+    echo "IPv6 does not work, disabling it."
+    export IPv6="false"
+fi
+
 
 if [ "$IPv4" = "false" ] && [ "$IPv6" = "false" ]; then
     echo "IPv4 and IPv6 disabled, stopping."
     sleep inf
 fi
 
-
 while true; do
-    if [ "$IPv4" = "true" ]; then
-        curl -sSL4 https://ipv64.net/update.php?key="$DUK" | tee /tmp/IPv4.json
+    if [ "$IPv4" = "true" ] && [ "$(curl -sS4 https://ipv4.ipv64.net/ipcheck.php?ipv4)" != "$(dig "$DUD" IN A +short +https +tls-ca=/etc/ssl/certs/ca-certificates.crt @1.1.1.1 | grep '^[0-9.]\+$' | sort | head -n1)" ]
+        curl -sSL4 https://ipv4.ipv64.net/update.php?key="$DUK"&domain="$DUD" | tee /tmp/IPv4.json
     fi
-    if [ "$IPv6" = "true" ]; then
-        curl -sSL6 https://ipv64.net/update.php?key="$DUK" | tee /tmp/IPv6.json
+    if [ "$IPv6" = "true" ] && [ "$(curl -sS6 https://ipv6.ipv64.net/ipcheck.php?ipv6)" != "$(dig "$DUD" IN AAAA +short +https +tls-ca=/etc/ssl/certs/ca-certificates.crt @1.1.1.1 | grep '^[0-9a-f:]\+$' | sort | head -n1)" ]
+        curl -sSL6 https://ipv6.ipv64.net/update.php?key="$DUK"&domain="$DUD" | tee /tmp/IPv6.json
     fi
 
     sleep "$UI"
